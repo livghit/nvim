67f50619a7c5e96e8aa3fd1e40ac0bb4022a2101
livghit <liviu.g01@gmail.com>
2023-11-15 11:41:44 +0100

    Style change Spaces to Tabs

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = function()
-    return {
-      transparent = true,
-      terminal_colors = true,
-      styles = {
-        sidebars = "transparent",
-      },
-      lualine_bold = true,
-    }
-  end,
+	"craftzdog/solarized-osaka.nvim",
+	lazy = true,
+	priority = 1000,
+	opts = function()
+		return {
+			transparent = true,
+			terminal_colors = true,
+			styles = {
+				sidebars = "transparent",
+			},
+			lualine_bold = true,
+		}
+	end,
 }
