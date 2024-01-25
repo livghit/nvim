426863ed25efedfd4dd0afaa8b927c88ccf8a208
livghit <liviu.g01@gmail.com>
2024-01-25 21:00:03 +0100

    removed copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,7 +25,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.go" },
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
-		{ import = "lazyvim.plugins.extras.coding.copilot" },
+		--{ import = "lazyvim.plugins.extras.coding.copilot" },
 		--{ import = "lazyvim.plugins.extras.ui.mini-animate" },
 		-- import/override with your plugins
 		{ import = "plugins" },
