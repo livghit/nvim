89cdf6d1432dd8f55fb569b9e2b571a2e268ac1c
Liviu Ghita <liviu.ghita@sicor.de>
2025-03-18 15:38:57 +0100

    [WIP] Splitted all the configs into own files

--- lua/custom/plugins/nvim-web-devicons.lua
+++
@@ -1,4 +0,0 @@
-require('nvim-web-devicons').set_icon {
-  lua = { icon = '', color = '#69e8ff', name = 'Lua' },
-  go = { icon = '󰟓', color = '#69e8ff', name = 'Go' },
-}
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -22,4 +22,10 @@ return {
       },
     },
   },
+  config = function()
+    require('nvim-web-devicons').set_icon {
+      lua = { icon = '', color = '#69e8ff', name = 'Lua' },
+      go = { icon = '󰟓', color = '#69e8ff', name = 'Go' },
+    }
+  end,
 }
