9ac0343d6f36e90fb51dfb7605f488848ba18711
Liviu Ghita <liviu.ghita@sicor.de>
2025-03-21 15:18:31 +0100

    [Feature]🎣 Harpoon keymap update

--- lua/livghit/plugins/harpoon.lua
+++ lua/livghit/plugins/harpoon.lua
@@ -10,30 +10,30 @@ return {
 
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
-    end)
+    end, { desc = '[H]arpoon add to list' })
     vim.keymap.set('n', '<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    end, { desc = '[H]arpoon toogle menu' })
 
     vim.keymap.set('n', '<C-h>', function()
       harpoon:list():select(1)
-    end)
+    end, { desc = '[H]arpoon select first' })
     vim.keymap.set('n', '<C-j>', function()
       harpoon:list():select(2)
-    end)
+    end, { desc = '[H]arpoon select second' })
     vim.keymap.set('n', '<C-k>', function()
       harpoon:list():select(3)
-    end)
+    end, { desc = '[H]arpoon select third' })
     vim.keymap.set('n', '<C-l>', function()
       harpoon:list():select(4)
-    end)
+    end, { desc = '[H]arpoon select forth' })
 
     -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set('n', '<C-p>', function()
       harpoon:list():prev()
-    end)
+    end, { desc = '[H]arpoon previous' })
     vim.keymap.set('n', '<C-n>', function()
       harpoon:list():next()
-    end)
+    end, { desc = '[H]arpoon next' })
   end,
 }
