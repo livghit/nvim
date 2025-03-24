813d074c32d1b11666643927a2cd377832b17b23
livghit <liviu.g01@gmail.com>
2025-03-24 17:17:21 +0100

    Autochange dark mode light mode

--- lua/livghit/plugins/colorscheme.lua
+++ lua/livghit/plugins/colorscheme.lua
@@ -1,3 +1,24 @@
+function is_dark_mode()
+  if vim.fn.has 'mac' then
+    local handle = io.popen 'defaults read -g AppleInterfaceStyle 2>/dev/null'
+    local result = handle:read '*a'
+    handle:close()
+    return result:match 'Dark' ~= nil
+  elseif vim.fn.has 'unix' == 1 then
+    local handle = io.popen 'gsettings get org.gnome.desktop.interface color-scheme 2>/dev/null'
+    local result = handle:read '*a'
+    handle:close()
+    return result:match 'dark' ~= nil
+  elseif vim.fn.has 'win32' == 1 then
+    local handle =
+      io.popen [[powershell -command "& {Get-ItemPropertyValue -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme}"]]
+    local result = handle:read '*a'
+    handle:close()
+    return tonumber(result) == 0 -- 0 means dark mode
+  end
+  return false -- Default to light mode
+end
+
 return {
   -- You can easily change to a different colorscheme.
   -- Change the name of the colorscheme plugin below, and then
@@ -9,7 +30,11 @@ return {
     name = 'github-theme',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'github_light'
+      if is_dark_mode() then
+        vim.cmd.colorscheme 'github_dark_dimmed'
+      else
+        vim.cmd.colorscheme 'github_light'
+      end
     end,
   },
   {
